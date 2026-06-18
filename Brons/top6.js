eda4eb851501b6b12b751488d9dbe69aacc66176
markmarkmarkt <markzero62@gmail.com>
2026-06-18 09:14:42 +0200

    Create top6.js

---
+++ Brons/top6.js
@@ -0,0 +1,20 @@
+// opdracht 1
+const top_6 = [
+    "stupid song",
+    "hate that i made you love me",
+    "Billie Jean",
+    "SWIM",
+    "Beauty And A Beat",
+    "Babydoll"
+]
+
+// opdracht 2
+console.log(top_6[0]);
+
+// opdracht 2
+console.log(top_6[2]);
+
+// opdracht 3
+top_6.forEach(song => {
+    console.log(song);
+});
